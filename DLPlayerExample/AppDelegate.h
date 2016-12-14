43fcf3d2164ca8ce75f2629c564a8c2140d40014
ZengyiMa <mazengyi1991@gmail.com>
2016-12-14 14:23:41 +0800

    init code

---
+++ DLPlayerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLPlayerExample
+//
+//  Created by famulei on 14/12/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
